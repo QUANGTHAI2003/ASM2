044285f3220bb7e1b04130799ecab6ad87263d5e
Trần Quang Thái <tranquangthai.10102003@gmail.com>
2022-05-20 16:40:20 +0700

    asm2

--- js/app.js
+++ js/app.js
@@ -3,3 +3,8 @@ $('.set-bg').each(function () {
     var bg = $(this).data('setbg');
     $(this).css('background-image', 'url(' + bg + ')');
 });
+
+// Loading
+$(window).on('load', function () {
+    $('.loader-wrapper').fadeOut('slow');
+})
